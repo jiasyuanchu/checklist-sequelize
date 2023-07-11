52253f2294c79c1d01147069aad691f807c3227b
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-11 15:44:19 +0800

    feat: build up basic views and routers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 
+const routes = require('./routes')
+
 const app = express()
 const PORT = 3000
 
@@ -13,6 +15,9 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
+app.use(routes)
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
