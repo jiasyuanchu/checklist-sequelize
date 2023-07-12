606e8dc575e501111d3fd81783842190c61aae21
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-12 23:45:29 +0800

    feat: add home page

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
 
 app.use(routes)
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../../models')
+const Item = db.Item
+const User = db.User
+
 router.get('/', (req, res) => {
-  res.render('index')
+  return Item.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((items) => { return res.render('index', { items: items }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 module.exports = router
